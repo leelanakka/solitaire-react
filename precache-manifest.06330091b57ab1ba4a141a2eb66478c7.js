df303bbf077868be84ef706e2db94e99f98d1844
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-16 20:11:29 +0530

    Updates

---
+++ precache-manifest.06330091b57ab1ba4a141a2eb66478c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92151683480ea71bf3a1",
+    "url": "/solitaire-react/static/css/main.b6794b7f.chunk.css"
+  },
+  {
+    "revision": "92151683480ea71bf3a1",
+    "url": "/solitaire-react/static/js/main.0eb9addf.chunk.js"
+  },
+  {
+    "revision": "423a1cf31494fdd5629d",
+    "url": "/solitaire-react/static/js/runtime~main.a8e704d3.js"
+  },
+  {
+    "revision": "d9a0346482615cb312bf",
+    "url": "/solitaire-react/static/js/2.761727aa.chunk.js"
+  },
+  {
+    "revision": "98cb8e90fabd0478c596e9c988a35623",
+    "url": "/solitaire-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/solitaire-react/precache-manifest.06330091b57ab1ba4a141a2eb66478c7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/solitaire-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
